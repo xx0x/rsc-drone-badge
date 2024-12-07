d943ada99d9a0b34f1d45938df9f616835a01c6d
Vaclav Mach <vaclavmach@hotmail.cz>
2024-12-07 13:04:41 +0100

    add attiny85 build (same code)

--- build/upload.bat
+++ build/upload.bat
@@ -1,10 +1,22 @@
 @echo off
 
+REM Ask whether to use attiny25 or attiny85
+set /p MCU_SUFFIX=Which ATTINY to use (25/85)? 
+
+if "%MCU_SUFFIX%"=="25" (
+    set MCU=attiny25
+    set BIN_FILE=RscDroneBadge_attiny25.hex
+) else if "%MCU_SUFFIX%"=="85" (
+    set MCU=attiny85
+    set BIN_FILE=RscDroneBadge_attiny85.hex
+) else (
+    echo Invalid ATTINY selected, write 25 or 85. Exiting.
+    exit /b 1
+)
+
 REM Define variables
 set AVRDUDE=avrdude
 set PROGRAMMER=usbasp
-set MCU=attiny25
-set BIN_FILE=RscDroneBadge.hex
 set SPEED=93750Hz
 
 REM Check if avrdude exists
@@ -25,5 +37,30 @@ echo.
 %AVRDUDE% -B %SPEED% -c %PROGRAMMER% -p %MCU% -U flash:w:%BIN_FILE%
 echo All done!
 echo.
+
+:ask_again
+set /p yn=Do you want to flash it again (yes=default/no)? 
+if "%yn%"=="" set yn=yes
+
+if /i "%yn%"=="yes" (
+    REM Set fuses
+    echo Setting fuses...
+    echo.
+    %AVRDUDE% -B %SPEED% -c %PROGRAMMER% -p %MCU% -e -Uefuse:w:0xFF:m -Uhfuse:w:0b11010111:m -Ulfuse:w:0xE2:m
+
+    REM Upload firmware
+    echo Uploading firmware...
+    echo.
+    %AVRDUDE% -B %SPEED% -c %PROGRAMMER% -p %MCU% -U flash:w:%BIN_FILE%
+    echo All done!
+    echo.
+    goto ask_again
+) else if /i "%yn%"=="no" (
+    echo Exiting.
+    exit /b
+) else (
+    echo Please answer yes or no.
+    goto ask_again
+)
 
 pause
--- build/upload.sh
+++ build/upload.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# Ask whether to use attiny25 or attiny85
+read -p "Which ATTINY to use (25/85)? " MCU_SUFFIX
+
+if [ "$MCU_SUFFIX" == "25" ]; then
+    MCU="attiny25"
+    BIN_FILE="RscDroneBadge_attiny25.hex"
+elif [ "$MCU_SUFFIX" == "85" ]; then
+    MCU="attiny85"
+    BIN_FILE="RscDroneBadge_attiny85.hex"
+else
+    echo "Invalid ATTINY selected, write 25 or 85. Exiting."
+    exit 1
+fi
+
 # Define variables
-AVRDUDE="avrdude"                  # Path to avrdude executable
-PROGRAMMER="usbasp"                # Programmer type
-MCU="attiny25"                     # Target microcontroller
-BIN_FILE="RscDroneBadge.hex" # Firmware binary file
-SPEED="93750Hz"                    # Programmer speed
+AVRDUDE="avrdude"   # Path to avrdude executable
+PROGRAMMER="usbasp" # Programmer type
+SPEED="93750Hz"     # Programmer speed
 
 # Set fuses
 echo "Setting fuses..."
@@ -18,3 +30,29 @@ echo ""
 $AVRDUDE -B $SPEED -c $PROGRAMMER -p $MCU -U flash:w:$BIN_FILE
 echo "All done!"
 echo ""
+
+# Ask whether to flash it again
+while true; do
+    read -p "Do you want to flash it again (yes=default/no)? " yn
+    yn=${yn:-yes} # Default to yes if enter is pressed
+    case $yn in
+    [Yy]*)
+        # Set fuses
+        echo "Setting fuses..."
+        echo ""
+        $AVRDUDE -B $SPEED -c $PROGRAMMER -p $MCU -e -Uefuse:w:0xFF:m -Uhfuse:w:0b11010111:m -Ulfuse:w:0xE2:m
+
+        # Upload firmware
+        echo "Uploading firmware..."
+        echo ""
+        $AVRDUDE -B $SPEED -c $PROGRAMMER -p $MCU -U flash:w:$BIN_FILE
+        echo "All done!"
+        echo ""
+        ;;
+    [Nn]*)
+        echo "Exiting."
+        exit
+        ;;
+    *) echo "Please answer yes or no." ;;
+    esac
+done
